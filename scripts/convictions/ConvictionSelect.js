6f8a11a1a8c5e685eaac77f4089e0b35e9c19388
Shawn Conerton <conerton52@gmail.com>
2020-10-21 11:16:33 -0500

    Added code and event for conviction. Unsure if working.

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -7,15 +7,26 @@ import { useConvictions, getConvictions } from "./ConvictionProvider.js"
 
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".filters__crime")
+const eventHub = document.querySelector(".filters__crime")
 
-export const ConvictionSelect = () => {
-    // Get all convictions from application state
-    getConvictions().then( () => {
-    
-    const convictions = useConvictions()
-    render(convictions)
-    })
-}
+eventHub.addEventListener("change", event => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+
+
 
 const render = convictionsCollection => {
     /*
@@ -36,4 +47,15 @@ const render = convictionsCollection => {
             }
         </select>
     `
+}
+
+
+export const ConvictionSelect = () => {
+    // Get all convictions from application state
+    getConvictions()
+        .then( () => {
+    
+            const convictions = useConvictions()
+            render(convictions)
+    })
 }
